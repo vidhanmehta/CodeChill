8e4d895ca40c4d0a6a9d4db2663ff8d68d6f988d
divyesh123-jain <divyeshjain318@gmail.com>
2024-02-04 02:35:32 +0530

    created the Analysis Page and components

--- app/Analysis/page.tsx
+++ app/Analysis/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
+import SideEffectsCards from '@/components/SideEffectsCards';
 
-const Analysis:React.FC = () => {
-    return (<>
-        <div className="flex h-screen w-screen">
-            <div className="flex flex-1 w-full pragyam"></div>
-            <div className="flex flex-1 w-full divyesh"></div>
-        </div>
-        </>
-    );
+const Analysis: React.FC = () => {
+
+
+  return (
+    <>
+      <div className="flex  w-screen bg-white">
+        <div className="flex flex-1 w-full pragyam"></div>
+        <div className="flex flex-1 flex-row w-full justify-center pt-16 ">
+     <SideEffectsCards />
+  
+
+        </div>  
+      </div>
+      
+    </>
+  );
 };
 
 export default Analysis;
