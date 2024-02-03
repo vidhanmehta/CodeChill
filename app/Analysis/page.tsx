90e77db43e34d7524108b7dfdf781e91ddb067f3
prag-man <beyondyourunderstanding479@gmail.com>
2024-02-04 01:29:29 +0530

    create another api route for testing

--- app/Analysis/page.tsx
+++ app/Analysis/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Rubik } from 'next/font/google';
+import Circ
+
+const rubik = Rubik({
+    subsets: ['latin-ext']
+})
 
 const Analysis:React.FC = () => {
     return (<>
@@ -6,7 +12,7 @@ const Analysis:React.FC = () => {
             <div className="flex-row flex-1 w-full px-10 py-8 pragyam">
                 <h2  className='text-3xl font-semibold text-rose-700 bg-slate-100 h-fit w-fit p-2 -rotate-6 mt-4'>Not a Wise Choice!</h2>
                 <div className='flex mt-6 ml-8'>
-                    <h3 className='text-4xl font-'>Product Name</h3>
+                    <h3 className={`text-4xl ${rubik.className} mt-4 `}>Product Name</h3>
                 </div>
             </div>
             <div className="flex flex-1 w-full divyesh"></div>
