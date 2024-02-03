8c6e5f2ba764eb4462b3d7e827c2b607e8347875
prag-man <beyondyourunderstanding479@gmail.com>
2024-02-04 00:34:54 +0530

    frontend starts

--- app/Analysis/page.tsx
+++ app/Analysis/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Analysis:React.FC = () => {
-    return (
-       <>
-       
-       </>
+    return (<>
+        <div className="flex h-screen w-screen">
+            <div className="flex flex-1 w-full pragyam"></div>
+            <div className="flex flex-1 w-full divyesh"></div>
+        </div>
+        </>
     );
 };
 
