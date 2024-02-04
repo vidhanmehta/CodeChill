67bfaea206bb453509bbf76bfb3c20dd5e566b23
prag-man <beyondyourunderstanding479@gmail.com>
2024-02-04 05:38:09 +0530

    Designed landing page

--- app/Analysis/page.tsx
+++ app/Analysis/page.tsx
@@ -1,28 +1,14 @@
-import React from 'react';
+"use client"
 import { Rubik } from 'next/font/google';
 import {CircularProgress} from "@nextui-org/react";
 import {Chip} from "@nextui-org/react";
 import {Slider} from "@nextui-org/react";
-"use client"
 import React, { useState, useEffect } from 'react';
 import SideEffectsCards from '@/components/SideEffectsCards';
 
-const Analysis: React.FC = () => {
-
-
-  return (
-    <>
-      <div className="flex  w-screen bg-white">
-        <div className="flex flex-1 w-full pragyam"></div>
-        <div className="flex flex-1 flex-row w-full justify-center pt-16 ">
-     <SideEffectsCards />
-  
-
-        </div>  
-      </div>
-      
-    </>
-  );
+const rubik = Rubik({
+    subsets:['latin-ext']
+})
 
 
 const Analysis:React.FC = () => {
@@ -91,7 +77,9 @@ const Analysis:React.FC = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-1 w-full divyesh"></div>
+            <div className="flex flex-1 w-full divyesh">
+                <SideEffectsCards />
+            </div>
         </div>
         </>
     );
