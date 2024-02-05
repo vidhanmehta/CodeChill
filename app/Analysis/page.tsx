18d8edc1444aa0a0602714a38135a088b1a814e4
divyesh123-jain <divyeshjain318@gmail.com>
2024-02-05 16:39:26 +0530

    Co-authored-by: Partha Priyadarshi Das <MrSnor@users.noreply.github.com>

--- app/Analysis/page.tsx
+++ app/Analysis/page.tsx
@@ -13,6 +13,15 @@ import axios from 'axios';
 import { SparklesPreview } from '@/components/Loader';
 import toast from 'react-hot-toast';
 
+
+interface AnalysisProps {
+    product_title: string;
+    product_desription: string;
+    safety_score: number;
+    reviews_sentiment_analysis: {
+        sentiment_score: number;
+    }
+}
 const rubik = Rubik({
     subsets:["latin"]
 });
@@ -25,16 +34,17 @@ const Analysis:React.FC = () => {
     const link = searchParams.get("link")
     const filters = searchParams.get("filters")
     console.log(link, filters)
-    const [response, setResponse] = useState()
+    const [response, setResponse] = useState<AnalysisProps | undefined>();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () =>{
         try {
-            const res = await axios.post('http://localhost:3000/api/analysis', {link: link, filters: filters}).then(res => {
+            const result = await axios.post('http://localhost:3000/api/analysis', {link: link, filters: filters}).then(res => {
                 setLoading(false);
                 console.log(res.data)
                 setResponse(res.data)
+                console.log(response)
                 if(res.data.safety_score! == 0){
                     toast.error("Contains one or more of the filters and hence the safety score is brought down to 0")
                 }
@@ -49,11 +59,14 @@ const Analysis:React.FC = () => {
     fetchData()
     },[])
 
+    console.log(response?.product_title)
+
+   
     return (<> {loading && <SparklesPreview />} {!loading && <>  
         <div className="flex h-screen background__container w-screen ">
             <div className="flex-row flex-1 w-full px-10 py-8 pragyam">
                 {(response!.safety_score <= 50) && <h2  className='text-3xl font-semibold text-rose-700 bg-[#eec0c0] h-fit w-fit p-2 -rotate-12 mt-4'>Not a Wise Choice!</h2>}
-                {(80 > response!.safety_score > 50) && <h2  className='text-3xl font-semibold text-yellow-500 bg-[#e8efa58c] h-fit w-fit p-2 -rotate-12 mt-4'>Good Choice!</h2>}
+                {(response!.safety_score > 50 && response!.safety_score < 80) && <h2  className='text-3xl font-semibold text-yellow-500 bg-[#e8efa58c] h-fit w-fit p-2 -rotate-12 mt-4'>Good Choice!</h2>}
                 {(response!.safety_score >= 80) &&<h2  className='text-3xl font-semibold text-green-500 bg-[#d2fbae] h-fit w-fit p-2 -rotate-12 mt-4'>It's a Great Choice!</h2>}
                 <div className='flex mt-6 ml-8 w-full justify-evenly'>
                     <h1 className='text-4xl border-b-4 border-[#BA7969] mt-4 '>{response!.product_title!}</h1>
@@ -68,7 +81,7 @@ const Analysis:React.FC = () => {
                         showValueLabel={true}
                         />
                 </div>
-                <p className='text-md mt-4 text-[0.8rm] analysis__container font-semibold'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p className='text-md mt-4 text-[0.8rm] analysis__container font-semibold'>{response!.product_desription}</p>
                 <div className='flex gap-6 pt-5'>
                     <div className="flex-1 flex-row mt-4  pt-4">
                         <h4>Ingredients</h4>
@@ -119,7 +132,7 @@ const Analysis:React.FC = () => {
                     defaultValue={response!.reviews_sentiment_analysis?.sentiment_score}
                     className="max-w-md"
                     />
-                    <p className='mt-10 analysis__container'>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Cont</p>
+                    <p className='mt-10 analysis__container'>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Cont</p>
                     </div>
                 </div>
             </div>
